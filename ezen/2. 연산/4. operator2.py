9db99ef6134f3ecc720acae143d62e7a2fbce213
yeongeun-Kim <1787forwork@gmail.com>
2022-11-21 13:00:36 +0900

    1121 Python push

---
+++ ezen/2. 연산/4. operator2.py
@@ -0,0 +1,18 @@
+# 비교연산
+print(2>3) #False
+print(15<38) #True
+print(1.5 >= 0) #True
+print(3 <= 3)
+print("주변음" == "주변옴")
+print("1111111111" == "1111111111")
+
+# 논리연산
+print( 4 < 6 and 10 >= 10) #True and True => True
+print("적용형 주변음 허용 모드" != "적용형 주변음 허용 모드" or "나는 할 수 있다" == "나는 할 수 있다") #True
+print(not 5 == 5)
+
+# 멤버십 연산자
+# in - 포함되어 있다
+# not in - 포함되어 있지 않다
+print("a" in "abc")
+print("d" not in "abc")
